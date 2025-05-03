e9be234ce9162d1d39df648829c7dba424d90dc4
bless5190 <giovani.marth@gmail.com>
2025-05-03 14:19:12 -0300

    Update binance.js

--- pages/api/binance.js
+++ pages/api/binance.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Preflight request
+  }
+
   try {
     const response = await fetch('https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search', {
       method: 'POST',
